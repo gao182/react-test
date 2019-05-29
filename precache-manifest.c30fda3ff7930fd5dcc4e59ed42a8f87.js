5ba70db651dbe4acfa6905f12466ad91cdbe91cd
gao182 <gaoxi182@163.com>
2019-05-29 22:49:00 +0800

    Updates

---
+++ precache-manifest.c30fda3ff7930fd5dcc4e59ed42a8f87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "876ec5f0e03ab0d26f55f76cae8ed46b",
+    "url": "/react-tomato/index.html"
+  },
+  {
+    "revision": "71365886847ee874bb24",
+    "url": "/react-tomato/static/css/2.2a88c607.chunk.css"
+  },
+  {
+    "revision": "e5d557e7935adf098e6b",
+    "url": "/react-tomato/static/css/main.cfece337.chunk.css"
+  },
+  {
+    "revision": "71365886847ee874bb24",
+    "url": "/react-tomato/static/js/2.9d1c7924.chunk.js"
+  },
+  {
+    "revision": "e5d557e7935adf098e6b",
+    "url": "/react-tomato/static/js/main.c1a367ff.chunk.js"
+  },
+  {
+    "revision": "3395afd7058abe3c3f3e",
+    "url": "/react-tomato/static/js/runtime~main.3a0aa504.js"
+  },
+  {
+    "revision": "9ffc74fb2f3e0ac4c9865fe70289831c",
+    "url": "/react-tomato/static/media/logo200.9ffc74fb.png"
+  }
+]);
--- src/Redux/actionType.js
+++
@@ -1,8 +0,0 @@
-export const ADD_TODO = 'add_todos'
-export const INIT_TODOS = 'init_todos'
-export const UPDATE_TODO = 'update_todo'
-export const EDIT_TODO = 'edit_todo'
-
-export const ADD_TOMATO = 'add_tomato'
-export const UPDATE_TOMATO = 'update_tomato'
-export const INIT_TOMATOES = 'init_tomatos'
